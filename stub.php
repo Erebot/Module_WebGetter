c4ea2fbd45a31d826cd1d32f3e0e4fa31d65bd83
François Poirotte <clicky@erebot.net>
2013-01-20 20:46:07 +0100

    Fix stub.

--- stub.php
+++ stub.php
@@ -1,1 +1,62 @@
-buildenv/stub.php
+<?php
+/*
+    This file is part of Erebot.
+
+    Erebot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Erebot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+if (version_compare(phpversion(), '5.3.1', '<')) {
+    if (substr(phpversion(), 0, 5) != '5.3.1') {
+        // this small hack is because of running RCs of 5.3.1
+        echo "@PACKAGE_NAME@ requires PHP 5.3.1 or newer." . PHP_EOL;
+        exit(1);
+    }
+}
+foreach (array('phar', 'spl', 'pcre', 'simplexml') as $ext) {
+    if (!extension_loaded($ext)) {
+        echo "Extension $ext is required." . PHP_EOL;
+        exit(1);
+    }
+}
+try {
+    Phar::mapPhar();
+} catch (Exception $e) {
+    echo "Cannot process @PACKAGE_NAME@ phar:" . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+    exit(1);
+}
+
+// Return metadata about this package.
+$metadata = json_decode(
+    file_get_contents(
+        "phar://" . __FILE__ .
+        DIRECTORY_SEPARATOR . "@PACKAGE_NAME@-@PACKAGE_VERSION@" .
+        DIRECTORY_SEPARATOR . "data" .
+        DIRECTORY_SEPARATOR . "pear.erebot.net" .
+        DIRECTORY_SEPARATOR . "@PACKAGE_NAME@" .
+        DIRECTORY_SEPARATOR . "composer.json"
+    ),
+    TRUE
+);
+$metadata['version'] = '@PACKAGE_VERSION@';
+$metadata['extra']['PEAR']['name'] = '@PACKAGE_NAME@';
+$metadata['extra']['phar']['path'] =
+    "phar://" . __FILE__ .
+    DIRECTORY_SEPARATOR . "@PACKAGE_NAME@-@PACKAGE_VERSION@" .
+    DIRECTORY_SEPARATOR . "php";
+// Erebot_Module_WebGetter's main .phar embeds the code of its dependencies.
+$metadata['provide']['pear-pear.php.net/http_request2'] = '*';
+return $metadata;
+
+__HALT_COMPILER();
